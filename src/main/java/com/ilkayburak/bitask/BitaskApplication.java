0d70dd40d498e9ac8b4e3d736fea2d065e2a9862
= <=>
2024-08-07 16:54:21 +0300

    ++first initial commit

---
+++ src/main/java/com/ilkayburak/bitask/BitaskApplication.java
@@ -0,0 +1,13 @@
+package com.ilkayburak.bitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilkayburak/bitask/BitaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilkayburak.bitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
