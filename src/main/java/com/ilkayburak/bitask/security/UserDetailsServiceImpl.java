5777f39f29fcb8b62cd9971f645beb4f47e2dca0
= <=>
2024-08-09 11:34:25 +0300

    ++jwt implementation completed.

---
+++ src/main/java/com/ilkayburak/bitask/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ilkayburak.bitask.security;
+
+import com.ilkayburak.bitask.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+    return userRepository.findByEmail(userEmail).orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+    }
+}
