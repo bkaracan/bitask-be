612c8a8aeb15c886066b34cc56b2e5809a7fb44e
= <=>
2024-09-09 11:56:32 +0300

    ++remove usage of generic wildcard type

---
+++ src/main/java/com/ilkayburak/bitask/dto/RegistrationResponseDTO.java
@@ -0,0 +1,15 @@
+package com.ilkayburak.bitask.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RegistrationResponseDTO {
+
+  private String message;
+  private String username;
+  private String email;
+}
