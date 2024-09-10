073444062da6f2e8410f7abdddb5c06a302e040f
= <=>
2024-09-10 09:51:02 +0300

    ++ ci/cd pipeline test

--- src/main/java/com/ilkayburak/bitask/enumarations/JobTitleEnum.java
+++ src/main/java/com/ilkayburak/bitask/enumarations/JobTitleEnum.java
@@ -6,32 +6,31 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum JobTitleEnum {
+  BACKEND_DEVELOPER(1L, "Backend Developer"),
+  FRONTEND_DEVEOPER(2L, "Frontend Developer"),
+  BUSINESS_ANALYST(3L, "Business Analysit"),
+  DEVOPS(4L, "Devops"),
+  QA_ENGINEER(5L, "Q&A Engineer"),
+  SOFTWARE_ARCHITECTURE(6L, "Software Architecture");
 
-    BACKEND_DEVELOPER(1L, "Backend Developer"),
-    FRONTEND_DEVEOPER(2L, "Frontend Developer"),
-    BUSINESS_ANALYST(3L, "Business Analysit"),
-    DEVOPS(4L, "Devops"),
-    QA_ENGINEER(5L, "Q&A Engineer"),
-    SOFTWARE_ARCHITECTURE(6L, "Software Architecture");
+  private final Long code;
+  private final String name;
 
-    private final Long code;
-    private final String name;
-
-    public static JobTitleEnum getByCode(Long code) {
-        for (JobTitleEnum e : JobTitleEnum.values()) {
-            if (e.getCode().equals(code)) {
-                return e;
-            }
-        }
-        return null;
+  public static JobTitleEnum getByCode(Long code) {
+    for (JobTitleEnum e : JobTitleEnum.values()) {
+      if (e.getCode().equals(code)) {
+        return e;
+      }
     }
+    return null;
+  }
 
-    public static JobTitleEnum getByName(String name) {
-        for (JobTitleEnum e : JobTitleEnum.values()) {
-            if (e.getName().equalsIgnoreCase(name)) {
-                return e;
-            }
-        }
-        return null;
+  public static JobTitleEnum getByName(String name) {
+    for (JobTitleEnum e : JobTitleEnum.values()) {
+      if (e.getName().equalsIgnoreCase(name)) {
+        return e;
+      }
     }
+    return null;
+  }
 }
