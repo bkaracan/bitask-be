e8b27285a008506195ae3e4bf0f4ac3c7d69892a
= <=>
2024-09-10 16:41:45 +0300

    ++test metodu revize edildi.

--- src/test/java/com/ilkayburak/bitask/AuthenticationControllerTests.java
+++ src/test/java/com/ilkayburak/bitask/AuthenticationControllerTests.java
@@ -9,6 +9,7 @@ import com.ilkayburak.bitask.dto.RegistrationRequestDTO;
 import com.ilkayburak.bitask.dto.RegistrationResponseDTO;
 import com.ilkayburak.bitask.dto.core.ResponsePayload;
 import com.ilkayburak.bitask.enumarations.core.ResponseEnum;
+import com.ilkayburak.bitask.email.EmailService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,19 +17,23 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AuthenticationControllerTests {
 
   @Autowired private MockMvc mockMvc;
 
   @MockBean private AuthenticationService authenticationService;
-  
+
+  @MockBean private EmailService emailService;
+
   @Autowired private ObjectMapper objectMapper;
-  
+
   @Test
   void testRegisterEndpoint() throws Exception {
     // Test için bir RegistrationRequestDTO nesnesi oluşturalım
@@ -40,6 +45,10 @@ class AuthenticationControllerTests {
             .password("password123")
             .jobTitleId(1L)
             .build();
+
+    // Mock bağımlılıkları ayarla
+    Mockito.doNothing().when(emailService).sendEmail(
+        Mockito.anyString(), Mockito.anyString(), Mockito.any(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString());
 
     // Mock sonucu
     Mockito.when(authenticationService.register(Mockito.any(RegistrationRequestDTO.class)))
