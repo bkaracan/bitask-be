2a17988c5b4456aa7f8e6d0902984e92b93b687d
ilkayakyildiz <ilkayakyildiz@outlook.com.tr>
2024-09-18 14:57:22 +0300

    ++board endpoints & fixes

---
+++ src/main/java/com/ilkayburak/bitask/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.ilkayburak.bitask.service;
+
+import com.ilkayburak.bitask.dto.BoardDTO;
+import com.ilkayburak.bitask.dto.core.ResponsePayload;
+
+import java.util.List;
+
+public interface BoardService {
+
+    ResponsePayload<BoardDTO> save(BoardDTO boardDTO);
+    ResponsePayload<BoardDTO> getById(Long id);
+    ResponsePayload<List<BoardDTO>> getAllBoardsForUser(Long id);
+    ResponsePayload<List<BoardDTO>> getAllBoardsByCreator(Long id);
+
+}
