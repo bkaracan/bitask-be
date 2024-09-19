342b06a35b4b750d26ae502b5e3ef6a69369e2e6
ilkay.akyildiz <ilkay.akyildiz@vbt.com.tr>
2024-09-19 10:31:43 +0300

    ++unused import

--- src/main/java/com/ilkayburak/bitask/service/SprintService.java
+++ src/main/java/com/ilkayburak/bitask/service/SprintService.java
@@ -3,8 +3,6 @@ package com.ilkayburak.bitask.service;
 import com.ilkayburak.bitask.dto.SprintDTO;
 import com.ilkayburak.bitask.dto.core.ResponsePayload;
 
-import java.util.List;
-
 public interface SprintService {
 
     ResponsePayload<SprintDTO> save(SprintDTO sprintDTO);
